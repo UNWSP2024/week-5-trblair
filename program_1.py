fd294adddccd069c67b202cc7e793d0038b7a8bf
trblair <trblair@students.unwsp.edu>
2024-10-04 20:19:39 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -3,26 +3,19 @@
 # then converts that distance to miles.  The conversion formula is as follows:  
 # Miles = kilometers x 0.6214.   
 # The conversion must be done as a function with input and output.
-
-
 def kilometer_conversion(kilometers):    
-    miles = 0.0
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################   
+    miles = 0.0  
     miles=kilometers * 0.6214
-
-
     # Return the variable to the calling function
     return miles
-
 #### This piece of the code has been done for you,
 #### you only need to worry about the actual temp 
 #### conversion logic in the temp_conversion function
 if __name__ == '__main__':
     # Get User Input
-    result=float(input())
+    results=0
+    result=int(input())
     # Call kilometer_conversion
-    kilometer_conversion(result)
     # Display the miles
     print(kilometer_conversion(result))
+
